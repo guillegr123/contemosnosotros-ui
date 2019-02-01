1a7c67aa770dc5428d66f37f2f2601f957125100
masmerino <masmerino@gmail.com>
2019-02-01 13:45:16 -0600

    Candidate cards

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,24 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
+import { Card, Image, Divider } from "semantic-ui-react";
+import NumberFormat from "react-number-format";
+import styled, { ThemeProvider } from "styled-components";
+
+import results from "../../services/results.service";
 import background from "../../bg.jpg";
 
-const Manifest = () => {
+const CardWrapper = styled(Card)`
+  border-top: 2px solid ${props => props.bordercolor} !important;
+`;
+
+const CandidateCard = ({ candidate, votes }) => {
   return (
-    <React.Fragment>
-      <div className="pusher">
-        <div
-          className="ui inverted vertical masthead center aligned segment"
-          style={{ backgroundImage: `url(${background})` }}
-        >
-          <div className="ui raised very padded container segment">
-            <div className="text">
-              <h1 className="ui header">Resultados</h1>
-              TODO
-            </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
+    <CardWrapper bordercolor={candidate.color}>
+      <Card.Content>
+        <Image floated="right" size="mini" src={candidate.photo} />
+        <Card.Header>{candidate.name}</Card.Header>
+        <Card.Meta>{candidate.party}</Card.Meta>
+        <Card.Description>
+          <Divider horizontal>
+            <NumberFormat
+              value={votes}
+              displayType={"text"}
+              thousandSeparator={true}
+            />
+          </Divider>
+          25%
+        </Card.Description>
+      </Card.Content>
+    </CardWrapper>
   );
 };
 
-export default Manifest;
+class Results extends Component {
+  state = {
+    candidates: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      candidates: results
+    });
+  }
+
+  render() {
+    const { candidates } = this.state;
+
+    return (
+      <>
+        <div className="pusher">
+          <div
+            className="ui inverted vertical masthead center aligned segment"
+            style={{ backgroundImage: `url(${background})` }}
+          >
+            <div className="ui raised very padded container segment">
+              <Card.Group centered>
+                {candidates.map(({ candidate, votes }, key) => (
+                  <CandidateCard
+                    key={key}
+                    candidate={candidate}
+                    votes={votes}
+                  />
+                ))}
+              </Card.Group>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Results;
