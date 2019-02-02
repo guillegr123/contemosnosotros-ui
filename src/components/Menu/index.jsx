0a6f542699478c9e5b034c8b08d8c9232d500d41
Guillermo Gutierrez <guillegr123@gmail.com>
2019-02-01 22:47:13 -0600

    Uso de componentes NavLink para menú.

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,59 +1,28 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const Menu = withRouter(props => <InnerMenu {...props} />);
-
-const InnerMenu = props => {
-  const { pathname } = props.location;
-  const routes = [
-    {
-      text: "Inicio",
-      className: "item",
-      path: "/"
-    },
-    {
-      text: "Resultados",
-      className: "item",
-      path: "/resultados"
-    },
-    {
-      text: "Manifiesto",
-      className: "item",
-      path: "/manifiesto"
-    },
-    {
-      text: "FAQ",
-      className: "item",
-      path: "/faq"
-    }
-  ];
-
-  routes.forEach(route => {
-    if (route.path === pathname) {
-      route.className += " active";
-    }
-  });
-
-  return (
-    <div className="ui top borderless hidden inverted menu">
-      <div className="ui container">
-        <h1 className="header item">Elecciones 2019</h1>
-        <div className="right item">
-          <div className="ui secondary inverted pointing menu">
-            {routes.map((route, i) => (
-              <a
-                key={"menu-item-" + i.toString()}
-                href={route.path}
-                className={route.className}
-              >
-                {route.text}
-              </a>
-            ))}
-          </div>
+const Menu = props => (
+  <div className="ui top borderless hidden inverted menu">
+    <div className="ui container">
+      <h1 className="header item">Elecciones 2019</h1>
+      <div className="right item">
+        <div className="ui secondary inverted pointing menu">
+          <NavLink className="item" exact to="/">
+            Inicio
+          </NavLink>
+          <NavLink className="item" to="/resultados">
+            Resultados
+          </NavLink>
+          <NavLink className="item" to="/manifiesto">
+            Manifiesto
+          </NavLink>
+          <NavLink className="item" to="/faq">
+            FAQ
+          </NavLink>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Menu;
